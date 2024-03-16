f2b79ac8539e5c2d4449d6f67a80884a7923f307
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-16 15:54:56 +0100

    Prepared pdf for lab2

--- firstYear/secondSemester/MachineLearning/Lab/Lab2/project/fingerprints.py
+++ firstYear/secondSemester/MachineLearning/Lab/Lab2/project/fingerprints.py
@@ -1,6 +1,8 @@
 import numpy
+import sys
 import matplotlib
 import matplotlib.pyplot as plt
+numpy.set_printoptions(threshold=sys.maxsize)
 
 def mcol(v):
     return v.reshape((v.size, 1))
@@ -25,23 +27,92 @@ def hist_plot(D, L):
     D1 = D[:, L==1]
 
     hFea = {
-        0: 'False', 
-        1: 'True'
+        0: 'Feature 1', 
+        1: 'Feature 2',
+        2: 'Feature 3',
+        3: 'Feature 4',
+        4: 'Feature 5', 
+        5: 'Feature 6'
         }
 
-    for dIdx in range(2):
+    for dIdx in range(6):
         plt.figure()
         plt.xlabel(hFea[dIdx])
-        plt.hist(D0[dIdx, :], bins = 10, density= True, alpha = 0.4, label = 'False')
-        plt.hist(D0[dIdx, :], bins = 10, density= True, alpha = 0.4, label = 'True')
+        plt.hist(D0[dIdx, :], bins = 10, density= True, alpha = 0.4, label = 'Fake')
+        plt.hist(D1[dIdx, :], bins = 10, density= True, alpha = 0.4, label = 'Genuine')
         plt.legend()
         plt.tight_layout()
+        plt.savefig('output/hist_%d.png' % (dIdx+1))
     plt.show()
 
 def scatter_plot(D, L):
-    return 
+    D0 = D[:, L==0]
+    D1 = D[:, L==1]
+
+    hFea = {
+        0: 'Feature 1', 
+        1: 'Feature 2',
+        2: 'Feature 3',
+        3: 'Feature 4',
+        4: 'Feature 5', 
+        5: 'Feature 6'
+    }
+
+    for dIdx1 in range(6):
+        for dIdx2 in range(6):
+            if dIdx1 == dIdx2:
+                continue
+            plt.figure()
+            plt.xlabel(hFea[dIdx1])
+            plt.ylabel(hFea[dIdx2])
+            plt.scatter(D0[dIdx1, :], D0[dIdx2, :], label = 'Fake')
+            plt.scatter(D1[dIdx1, :], D1[dIdx2, :], label = 'Genuine')
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig('output/scatter_%d_%d.png' % ((dIdx1+1), (dIdx2+1)))
+        #plt.show()
+
 
 if __name__ == '__main__':
+    # Change default font size - comment to use default values
+    plt.rc('font', size=16)
+    plt.rc('xtick', labelsize=16)
+    plt.rc('ytick', labelsize=16)
+    
     D, L = load('trainData.txt')
-    print(L)
     hist_plot(D, L)
+    scatter_plot(D, L)
+
+    mu = D.mean(1).reshape((D.shape[0], 1))
+    print('Mean:')
+    print(mu)
+    print()
+
+
+    C = ((D - mu) @ (D - mu).T) / float(D.shape[1])
+    print('Covariance:')
+    print(C)
+    print()
+
+    var = D.var(1)
+    std = D.std(1)
+    print('Variance:', var)
+    print('Std. dev.:', std)
+    print()
+    
+
+
+    for cls in [0,1]:
+        print('Class', cls)
+        DCls = D[:, L==cls]
+        mu = DCls.mean(1).reshape(DCls.shape[0], 1)
+        print('Mean:')
+        print(mu)
+        C = ((DCls - mu) @ (DCls - mu).T) / float(DCls.shape[1])
+        print('Covariance:')
+        print(C)
+        var = DCls.var(1)
+        std = DCls.std(1)
+        print('Variance:', var)
+        print('Std. dev.:', std)
+        print()
---
+++ firstYear/secondSemester/MachineLearning/Lab/Lab2/project/try.typ
@@ -0,0 +1,55 @@
+= Laboratory 2 - Binary Classification
+
+
+#figure(
+    grid(
+        columns: 6,     // 2 means 2 auto-sized columns
+        rows: 6,
+        gutter: 2mm,    // space between columns
+        image("output/hist_1.png"),
+        image("output/scatter_1_2.png"), 
+        image("output/scatter_1_3.png"), 
+        image("output/scatter_1_4.png"), 
+        image("output/scatter_1_5.png"),
+        image("output/scatter_1_6.png"),
+
+
+        image("output/scatter_2_1.png"),
+        image("output/hist_2.png"),
+        image("output/scatter_2_3.png"), 
+        image("output/scatter_2_4.png"), 
+        image("output/scatter_2_5.png"),
+        image("output/scatter_2_6.png"),
+
+
+        image("output/scatter_3_1.png"), 
+        image("output/scatter_3_2.png"), 
+        image("output/hist_3.png"),
+        image("output/scatter_3_4.png"), 
+        image("output/scatter_3_5.png"),
+        image("output/scatter_3_6.png"),
+
+        image("output/scatter_4_1.png"), 
+        image("output/scatter_4_2.png"), 
+        image("output/scatter_4_3.png"), 
+        image("output/hist_4.png"),
+        image("output/scatter_4_5.png"),
+        image("output/scatter_4_6.png"),
+
+        image("output/scatter_5_1.png"), 
+        image("output/scatter_5_2.png"), 
+        image("output/scatter_5_3.png"), 
+        image("output/scatter_5_4.png"),
+        image("output/hist_5.png"),
+        image("output/scatter_5_6.png"),
+
+        image("output/scatter_6_1.png"), 
+        image("output/scatter_6_2.png"), 
+        image("output/scatter_6_3.png"), 
+        image("output/scatter_6_4.png"),
+        image("output/scatter_6_5.png"),
+        image("output/hist_6.png"),
+
+    ),
+    caption: "All features"
+)
