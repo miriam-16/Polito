3f558d207f18a1c20073e42530daf48bff07392e
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-17 11:42:04 +0100

    FInished exercise WebApp

--- firstYear/secondSemester/WebAppI/Labs/Lab1/filmLibrary.mjs
+++ firstYear/secondSemester/WebAppI/Labs/Lab1/filmLibrary.mjs
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 function Film(id, title, pid=1, rating = -1, favorite = false , date = null){
     this.id = id;
     this.title = title;
@@ -17,17 +18,17 @@ function FilmLibrary(){
             console.log('Title: %s', f.title)
             console.log('pId: %i', f.pid)
             console.log('Favorite: ', f.favorite)
-            console.log('Date: %s', f.date)
+            console.log('Date: %s', f.date == null ? 'Not defined' : f.date.toString())
             console.log('Rating: %i', f.rating)
         }
     };
 }
 
-const f1 = new Film(1,'Pulp Fiction',1,5, true,'2024-03-10');
-const f2 = new Film(2,'21 Grams',1,4, true,'2024-03-17');
+const f1 = new Film(1,'Pulp Fiction',1,5, true,dayjs('2024-03-10'));
+const f2 = new Film(2,'21 Grams',1,4, true,dayjs('2024-03-17'));
 const f3 = new Film(3,'Star Wars',1,0);
 const f4 = new Film(4,'Matrix',1,0);
-const f5 = new Film(5, 'Shrek',1, 3, undefined, '2024-03-21');
+const f5 = new Film(5, 'Shrek',1, 3, undefined, dayjs('2024-03-21'));
 
 const listFilms = new FilmLibrary();
 listFilms.addNewFilm(f1);
