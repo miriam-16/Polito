1d6bd496e15f53d766494a7d6facb43470ba06ad
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-19 11:18:40 +0100

    week3 exercise on db

--- firstYear/secondSemester/WebAppI/Exercises/Exercise1903/db.mjs
+++ firstYear/secondSemester/WebAppI/Exercises/Exercise1903/db.mjs
@@ -6,9 +6,22 @@ db.all("SELECT * FROM user", (err, rows) => { //it doesn't return nothing, requi
 
     //console.log(rows)
     rows.forEach((r) => {console.log(r.name)}) //can get attrubutes of the rows
-});
+}); //this callcback function is executed one time
 
-db.each("SELECT * FROM user", (err, row) => {
+db.each("SELECT * FROM user", (err, row) => { //select onlu one row
     if(err)
         throw err;
+
+        console.log(row.id, row.name)  //this is a callback executed fdor each element, so they might happen in any order
 })
+
+const userId = 3
+//avoid using concatenation
+db.get("SELECT * FROM USER WHERE id=?", [userId], (err, row) => {
+    console.log("USER IS ", row)
+})
+
+
+
+db.close() // be aware of when close the db connection, it must be after the execution of queries
+// we are scheduling two queries, we cannot ensure the order of the result because the order of the scheduler doesn't guarantee the order of the result
