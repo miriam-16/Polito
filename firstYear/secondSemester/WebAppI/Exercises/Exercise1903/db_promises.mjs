a646659faa4225f8cf342852254f0c9e711f72e5
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-20 17:18:43 +0100

    Promises in JS

---
+++ firstYear/secondSemester/WebAppI/Exercises/Exercise1903/db_promises.mjs
@@ -0,0 +1,20 @@
+import sqlite from 'sqlite3'
+const db = new sqlite.Database('questions.sqlite', (err) => {if(err) throw err});
+
+//focusing on computing the query and check if it succeedes or not
+const users = new Promise((resolve, reject) => {
+    db.all("select * from user", (err, rows) => {
+        if(err)
+            reject(err)
+        else 
+            resolve(rows)  //pratically, we are calling the function 'then'
+    })
+});
+
+console.log(users) //pending
+
+//when the execution of query starts, this print will be executed. This means I can use
+//users and its parameters
+users.then((rows) => {
+    console.log(rows)
+}).catch((err) => {console.log("Error", err)});
