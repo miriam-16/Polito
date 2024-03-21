38b82a03d9d56fdefbde60ef3063acedd4042a17
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-21 20:32:38 +0100

    solved

--- firstYear/secondSemester/WebAppI/Labs/Lab1/filmLibrary.mjs
+++ firstYear/secondSemester/WebAppI/Labs/Lab1/filmLibrary.mjs
@@ -96,8 +96,8 @@ this.insertMovie = function(f){
     return new Promise((resolve, reject) => {
         const sql = `insert into films(id, title, isFavorite, rating, watchDate, userId)
             values (?,?,?,?,?,?)`;
-        const isFavoriteInt = f.isFavorite ? 1 : 0;  // Convert boolean to integer
-        db.run(sql, [f.id, f.title, isFavoriteInt, f.rating, f.date.format('YYYY-MM-DD'), f.pid], (err) => {
+        
+        db.run(sql, [f.id, f.title, f.favorite ? 1: 0, f.rating, f.date.format('YYYY-MM-DD'), f.pid], (err) => {
             if(err) reject(err)
             else resolve()
         })
@@ -155,7 +155,7 @@ listFilms.addNewFilm(f5);
 const db = new sqlite3.Database('films_dump.db', (err) => {if(err) throw err});
 listFilms.getAllFilms().then((rows) => {
     const filmsAsArray = rows.map(row => new Film(row.id, row.title, row.userId, row.rating, row.isFavorite, row.watchedDate))
-    printAll(filmsAsArray)
+    //printAll(filmsAsArray)
 })
 
 listFilms.getFavorites().then((rows) => {
@@ -180,14 +180,12 @@ listFilms.getRatingGEThan(3).then((rows) => {
 
 listFilms.getSearchFilm("e").then((rows) => {
     const searchedArray = rows.map(row => new Film(row.id, row.title, row.userId, row.rating, row.isFavorite, row.watchDate))
-    printAll(searchedArray)
+    //printAll(searchedArray)
 })
 
-/* const f6 = new Film(12,'Dune',1,5, true,dayjs('2024-03-21'));
-listFilms.insertMovie(f6).then(() => {console.log("Movie added")})  */
+/*  const f6 = new Film(6,'Dune',1,5,true,dayjs('2024-03-21'));
+listFilms.insertMovie(f6).then(() => {console.log("Movie added")})   */
 
-listFilms.deleteMovie(7).then(() => {console.log("Movie removed")})
-listFilms.deleteMovie(8).then(() => {console.log("Movie removed")})
-listFilms.deleteMovie(12).then(() => {console.log("Movie removed")})
+//listFilms.deleteMovie(6).then(() => {console.log("Movie removed")})
 
-listFilms.setNullWatchDate().then(()=> {console.log("Set null all dates")})
+//listFilms.setNullWatchDate().then(()=> {console.log("Set null all dates")})
