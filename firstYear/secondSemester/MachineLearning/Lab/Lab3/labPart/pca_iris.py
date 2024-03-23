aaf183673e7d71da60362cd3789bcafe119dce96
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-23 19:53:15 +0100

    Implemented ML part1

--- firstYear/secondSemester/MachineLearning/Lab/Lab3/labPart/pca_iris.py
+++ firstYear/secondSemester/MachineLearning/Lab/Lab3/labPart/pca_iris.py
@@ -5,6 +5,32 @@ import matplotlib.pyplot as plt
 def mcol(v):
     return v.reshape((v.size, 1))
 
+def plot_scatter(D, L):
+    
+    D0 = D[:, L==0]
+    D1 = D[:, L==1]
+    D2 = D[:, L==2]
+
+    hFea = {
+        0: 'Sepal length',
+        1: 'Sepal width',
+        2: 'Petal length',
+        3: 'Petal width'
+        }
+
+    x = 0
+    y = 1
+
+    plt.figure()
+    plt.scatter(D0[x, :], D0[y, :], label = 'Setosa')
+    plt.scatter(D1[x, :], D1[y, :], label = 'Versicolor')
+    plt.scatter(D2[x, :], D2[y, :], label = 'Virginica')
+
+    plt.legend()
+    plt.tight_layout() # Use with non-default font size to keep axis label inside the figure
+    plt.savefig('scatter_%d_%d.pdf' % (x, y))
+    plt.show()  
+
 def load(fname):
     DList = []
     labelsList = []
@@ -49,5 +75,16 @@ if __name__ == '__main__':
 
     print("U")
     print(U)
+
+    m = 2
+
+    P = U[:,::-1][:, 0:m]
+    # U, s, Vh = numpy.linalg.svd(C)
+    # P = U[:, 0:m]
+    print(P)
+    DP = numpy.dot(P.T, D)
+    
+    plot_scatter(DP, L)
+    #print(DP)
 
     
