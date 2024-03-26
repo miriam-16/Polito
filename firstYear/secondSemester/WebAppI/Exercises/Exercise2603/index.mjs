1bc70cbd02189bc8d3d8e287caa329b8eb2442f5
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-26 11:25:29 +0100

    middlewares

--- firstYear/secondSemester/WebAppI/Exercises/Exercise2603/index.mjs
+++ firstYear/secondSemester/WebAppI/Exercises/Exercise2603/index.mjs
@@ -1,7 +1,10 @@
 import express from 'express'
+import morgan from 'morgan'
 
-const app = express()
-
+const app = express() 
+app.use(morgan('dev')) //Logging -> Print the request received
+app.use(express.static('public'))
+app.use(express.json)
 
 app.get('/', (req, res) => {
     res.send('Hello there')
