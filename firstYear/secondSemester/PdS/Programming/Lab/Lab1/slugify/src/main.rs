3abfbefc807df669d6aac33b9467ef41b6be054c
Miriam Lamari <miriam.lamari2@gmail.com>
2024-03-27 16:02:57 +0100

    TODO: gestire trattini

--- firstYear/secondSemester/PdS/Programming/Lab/Lab1/slugify/src/main.rs
+++ firstYear/secondSemester/PdS/Programming/Lab/Lab1/slugify/src/main.rs
@@ -1,24 +1,53 @@
 fn slugify(s: &str) -> String {
-    let lower_case = s.to_lowercase();
-    println!("{}", lower_case);
-    lower_case
+    let mut res = String::from("");
+    for ch in s.to_lowercase().chars(){
+        if ch.is_numeric() {
+            res.push(ch);
+        } else if ch.is_ascii_alphabetic() {
+            res.push(ch)
+        } else {
+            let prec = conv(ch);
+            if !res.ends_with("-"){
+                res.push(prec);
+            }
+        }
+    }
+    res
+
 }
 
-//returns:
 //  c se è uno ammesso
 //  la lettera non accentata corrispondente se viene trovata
 //  “-” negli altri casi
-/* fn conv(c: char) -> char {
+fn conv(c: char) -> char {
+    let mut res_converted: char = '-';
     const SUBS_I: &str =
         "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż";
     const SUBS_O: &str =
         "aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz";
 
+    /*     let converted_c = c.to_string().as_bytes();
 
-} */
+    const SUBS_I_converted: &[u8] = SUBS_I.as_bytes();
+    const SUBS_O_converted: &[u8] = SUBS_O.as_bytes(); */
+
+    for (c1, c2) in SUBS_I.chars().zip(SUBS_O.chars()) {
+        if c1 == c {
+            res_converted = c2;
+        }
+    }
+
+    res_converted
+}
 
 fn main() {
-    //println!("Hello, world!");
-    let s = String::from("Hello, world!");
-    slugify(&s);
+    let s = String::from("1Hellè, world!⚞");
+    print!("{}", slugify(&s));
+
+}
+
+#[test]
+fn my_first_test() {
+    // valore = preparazione test
+    assert_eq!(slugify("1Hellè, world!⚞"), "1helle--world--")
 }
