5b82b644a1ddad91d3c24a23ba6b42bd11c38de2
Miriam Lamari <miriam.lamari2@gmail.com>
2024-04-02 16:21:25 +0200

    finished

--- firstYear/secondSemester/PdS/Programming/Lab/Lab1/battaglianavale/src/main.rs
+++ firstYear/secondSemester/PdS/Programming/Lab/Lab1/battaglianavale/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::fs::{self, read_to_string};
 
 use clap::{Parser, Subcommand};
 
@@ -60,26 +60,91 @@ pub enum Boat {
 impl Board {
     /* crea una board vuota con una disponibilità di navi */
     pub fn new(boats: &[u8]) -> Board {
-/*         if boats.len() != bsize {
+        /*         if boats.len() != bsize {
             panic!("Boat count must be equal to bsize.");
         } */
         println!("{:?}", boats);
         Board {
-            
             boats: boats.try_into().unwrap(),
             data: [[0; bsize]; bsize],
         }
     }
 
+    /* crea una board a partire da una stringa che rappresenta tutto il contenuto del file board.txt */
+    pub fn from(s: String) -> Board {
+        let lines = s.split("\n");
+        let mut boats = [0u8; 4];
+        let mut data = [[0u8; bsize]; bsize];
 
-    /*     /* crea una board a partire da una stringa che rappresenta tutto il contenuto del file board.txt */
-    pub fn from(s: String) -> Board{}
+        for (i, line) in lines.enumerate() {
+            if i == 0 {
+                for (i, value) in line.split(" ").enumerate() {
+                    boats[i] = value.parse().unwrap();
+                }
+            } else {
+                for (j, c) in line.char_indices() {
+                    data[i - 1][j] = if c == ' ' { 0 } else { 1 };
+                }
+            }
+        }
+
+        Board { boats, data }
+    }
 
     /* aggiunge la nave alla board, restituendo la nuova board se possibile */
     /* BONUS: provare a non *non copiare* data quando si crea e restituisce una nuova board con la barca, come si può fare? */
-    pub fn add_boat(self, boat: Boat, pos: (usize, usize)) -> Result<Board, Error>{}
+    pub fn add_boat(self, boat: Boat, pos: (usize, usize)) -> Result<Board, Error> {
+        let mut res = self;
 
-     converte la board in una stringa salvabile su file */
+        match boat {
+            Boat::Vertical(n) => {
+                let end = pos.0 + n;
+                if end > bsize {
+                    return Err(Error::OutOfBounds);
+                }
+
+                if res.boats[n - 1] <= 0 {
+                    return Err(Error::BoatCount);
+                }
+
+                for i in pos.0..end {
+                    if res.data[i - 1][pos.1 - 1] !=0 {
+                        return Err(Error::Overlap);
+                    }
+                }
+
+                for i in pos.0..end {
+                    res.data[i - 1][pos.1 - 1] = 1;
+                }
+                res.boats[n - 1] -= 1;
+            }
+
+            Boat::Horizontal(n) => {
+                let end = pos.1 + n;
+                if end > bsize {
+                    return Err(Error::OutOfBounds);
+                }
+
+                if res.boats[n - 1] <= 0 {
+                    return Err(Error::BoatCount);
+                }
+
+                for i in pos.1..end {
+                    if res.data[pos.0 - 1][i - 1] != 0 {
+                        return Err(Error::Overlap);
+                    }
+                }
+
+                for i in pos.1..end {
+                    res.data[pos.0 - 1][i - 1] = 1;
+                }
+                res.boats[n - 1] -= 1;
+            }
+        }
+        Ok(res)
+    }
+
+    /* converte la board in una stringa salvabile su file */
     pub fn to_string(&self) -> String {
         let header = self
             .boats
@@ -104,7 +169,7 @@ impl Board {
     }
 }
 
-fn main(){
+fn main() {
     /*
     cargo run -- new board.txt 4,3,2,1
     questo crea una nuova board vuota nel file board.txt e può ospitare 4 navi da 1, 3
@@ -119,6 +184,7 @@ fn main(){
     let args = Args::parse();
 
     match &args.command {
+        // cargo run -- new -f board.txt -b 4,3,2,1
         Commands::New { file, boats } => {
             println!("{}", boats);
 
@@ -127,13 +193,15 @@ fn main(){
                     .split(",")
                     .map(|s| s.parse::<u8>().unwrap())
                     .collect::<Vec<_>>()
-                    .as_slice()
+                    .as_slice(),
             );
 
             let board_file = fs::write(file, board.to_string());
         }
-
+        //cargo run -- add -f board.txt -b 3V -s 10,10
         Commands::Add { file, boat, start } => {
+            let board = Board::from(read_to_string(file).unwrap());
+
             let (size, direction) = boat.split_at(1);
             let boat = match direction {
                 "V" => Boat::Vertical(size.parse().unwrap()),
@@ -142,12 +210,17 @@ fn main(){
             };
 
             let mut coordinates = start.split(",");
-            let x: i8 = coordinates.next().unwrap().parse().unwrap();
-            let y: i8 = coordinates.next().unwrap().parse().unwrap();
+            let x: usize = coordinates.next().unwrap().parse().unwrap();
+            let y: usize = coordinates.next().unwrap().parse().unwrap();
+            let coord = (x, y);
 
-            // Use x and y as needed
-
-            // Rest of the code
+            
+            match board.add_boat(boat, (x, y)) {
+                Err(e) => println!("Error"),
+                Ok(res) => {
+                    fs::write(file, res.to_string()).unwrap();
+                }
+            }
         }
     }
 }
