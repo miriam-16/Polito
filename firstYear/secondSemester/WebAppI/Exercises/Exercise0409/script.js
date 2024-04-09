5f495042a8f8df199bf9446604973f619b14204d
Miriam Lamari <miriam.lamari2@gmail.com>
2024-04-09 12:58:35 +0200

    script on ADD

--- firstYear/secondSemester/WebAppI/Exercises/Exercise0409/script.js
+++ firstYear/secondSemester/WebAppI/Exercises/Exercise0409/script.js
@@ -1,2 +1,7 @@
 console.log("We are here")              //will appear in the console section in the inspector
 console.log(window.location)
+
+const addButton = document.getElementById('addButton')
+addButton.addEventListener('click', () => {
+    console.log("You selected ADD")
+})
