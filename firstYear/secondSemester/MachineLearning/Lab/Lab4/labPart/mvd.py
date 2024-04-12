4922c2f7d30d199673d582788bfd786b9b4e5a12
Miriam Lamari <miriam.lamari2@gmail.com>
2024-04-12 14:01:16 +0200

    start lab4

---
+++ firstYear/secondSemester/MachineLearning/Lab/Lab4/labPart/mvd.py
@@ -0,0 +1,39 @@
+import numpy
+import matplotlib
+import matplotlib.pyplot as plt
+
+def mcol(v):
+    return v.reshape((v.size, 1))
+
+
+def load(fname):
+    DList = []
+    labelsList = []
+    hLabels = {
+        'Iris-setosa': 0,
+        'Iris-versicolor': 1,
+        'Iris-virginica': 2
+        }
+
+    with open(fname) as f:
+        for line in f:
+            try:
+                attrs = line.split(',')[0:-1]
+                attrs = mcol(numpy.array([float(i) for i in attrs]))
+                name = line.split(',')[-1].strip()
+                label = hLabels[name]
+                DList.append(attrs)
+                labelsList.append(label)
+            except:
+                pass
+
+    return numpy.hstack(DList), numpy.array(labelsList, dtype=numpy.int32)
+
+
+
+def main():
+    D, L = load('iris.csv')
+    
+
+if __name__ == "__main__":
+    main()
