d07c1cd16517118903811991b8676af696eec85f
Miriam Lamari <miriam.lamari2@gmail.com>
2024-05-08 13:08:39 +0200

    to complete

--- firstYear/secondSemester/PdS/Programming/Lab/Lab2/circularbuffer/src/main.rs
+++ firstYear/secondSemester/PdS/Programming/Lab/Lab2/circularbuffer/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fmt::{Error, Result}, ops::{Add, AddAssign, Index}};
+use std::{cell::Ref, fmt::{Error}, ops::{Add, AddAssign, Index}};
 
 fn main() {
     println!("Hello, world!");
@@ -9,7 +9,7 @@ pub struct CircularBuffer<T> {
     buffer: Vec<T>,
     head: usize,
     tail:usize,
-
+    size: usize,
 }
 
 impl<T> CircularBuffer<T> {
@@ -18,42 +18,53 @@ impl<T> CircularBuffer<T> {
             buffer: Vec::<T>::with_capacity(capacity),
             head: 0,
             tail: 0,
+            size: 0,
         }
     }
 
-    pub fn write(&mut self, item: T) -> Result<T, Error> {
-        if self.buffer.len() == self.buffer.capacity() {
+    pub fn write(&mut self, item: T) -> Result<(), &'static str> {
+        if self.size >= self.buffer.capacity() {
             // Buffer is full, overwrite the oldest element
-            self.head = 0;
-            self.tail = 0;
-            Err("No more space")
-        } else {
+            return Err("No more space");
+        }
             self.buffer.push(item);
             // Buffer is not full, append the item to the buffer
             self.tail = self.tail + 1;
             Ok(())
-        }
     }
 
 
 
-    pub fn read(&mut self) -> Option<T> {
+    pub fn read(&mut self) -> Option<&T> {
         if self.head == 0 {
-            None
-        } else {
-            self.head = self.head + 1;
-            Some(self.buffer[self.head -1])
+            return None;
         }
+        let old_head = self.head;
+        self.head = (self.head + 1) % self.buffer.capacity();
+        self.buffer.get(old_head)
+
+    }
+
+    pub fn clear(&mut self) {
+        self.buffer.clear();
+        self.size = 0;
+        self.head = 0;
+        self.tail = 0;
     }
 
 
+    pub fn size(&self) -> usize {
+        self.size
+    }
 
 
-    pub fn clear(&mut self) {};
-    pub dn size(&self) -> usize;
     // può essere usata quando il buffer è pieno per forzare una
     // scrittura riscrivendo l’elemento più vecchio
-    pub fn overwrite(&mut self, item: …) {};
+    pub fn overwrite(&mut self, item:T) {
+
+    }
+
+
     // vedi sotto*
     pub fn make_contiguos(&mut self) {};
 }
