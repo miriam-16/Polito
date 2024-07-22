bc769a155d1de314fd0107f2b8432df0295c46cb
Miriam Lamari <miriam.lamari2@gmail.com>
2024-07-22 15:30:50 +0200

    cleaning

--- firstYear/secondSemester/WebAppI/Exercises/Exercise0409/script.js
+++
@@ -1,7 +0,0 @@
-console.log("We are here")              //will appear in the console section in the inspector
-console.log(window.location)
-
-const addButton = document.getElementById('addButton')
-addButton.addEventListener('click', () => {
-    console.log("You selected ADD")
-})
